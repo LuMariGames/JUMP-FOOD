7085a9bb0d4145698abcfad61006ecf9aeaa9f78
LuMariGames <kai.hono426@gmail.com>
2024-12-21 07:53:08 +0900

    Update main.h

--- source/main.h
+++ source/main.h
@@ -1,16 +1,5 @@
 #pragma once
 
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <3ds.h>
-#include <citro2d.h>
-#include <limits.h>
-#include <math.h>
-#include <dirent.h>
-#include <unistd.h>
-#include <ctype.h>
-
 #define TOP_WIDTH  400
 #define TOP_HEIGHT 240
 #define BOTTOM_WIDTH  320
