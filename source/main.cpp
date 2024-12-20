a92b00e91446cc852a25344e47a2a08862a8d579
LuMariGames <kai.hono426@gmail.com>
2024-12-20 22:46:02 +0900

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -46,23 +46,23 @@ int main() {
 		C2D_TargetClear(top, C2D_Color32(0, 0, 0, 1));
 		C2D_SceneBegin(top);
 		// ステージ
-		C2D_DrawImageAtRotated(C2D_SpriteSheetGetImage(spriteSheet, 0),
+		C2D_DrawImageAt(C2D_SpriteSheetGetImage(spriteSheet, 0),
 			(float)(0.5),
 			(float)(0.5),
-			0,0.5f, NULL, 1.0f, 1.0f);
+			0.5f, NULL, 1.0f, 1.0f);
 
 		switch (scene) {
 		case 0:	//タイトル画面
 
-			C2D_DrawImageAtRotated(C2D_SpriteSheetGetImage(spriteSheet, 1),(float)(0.5),(float)(1/6),0.5f,0,NULL,1.0f,1.0f);
-			C2D_DrawImageAtRotated(C2D_SpriteSheetGetImage(spriteSheet, 2),(float)(0.5),(float)(0.625),0.5f,0,NULL,1.0f,1.0f);
+			C2D_DrawImageAtRotated(C2D_SpriteSheetGetImage(spriteSheet, 1),(float)(200),(float)(40),0.5f,0,NULL,1.0f,1.0f);
+			C2D_DrawImageAtRotated(C2D_SpriteSheetGetImage(spriteSheet, 2),(float)(200),(float)(150),0.5f,0,NULL,1.0f,1.0f);
 			if (key & KEY_A) scene = 1;
 
 			break;
 
 		case 1:	//ゲーム画面
 
-			C2D_DrawImageAtRotated(C2D_SpriteSheetGetImage(spriteSheet, 2),(float)(0.5),(float)(0.625),0.5f,0,NULL,1.0f,1.0f);
+			C2D_DrawImageAtRotated(C2D_SpriteSheetGetImage(spriteSheet, 2),(float)(200),(float)(150),0.5f,0,NULL,1.0f,1.0f);
 			if (key & KEY_X) ++count;
 
 			break;
