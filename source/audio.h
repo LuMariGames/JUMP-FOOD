5fae45fda77d7b2d9831133842e90f49885e0512
LuMariGames <kai.hono426@gmail.com>
2024-12-21 12:39:50 +0900

    Add files via upload

---
+++ source/audio.h
@@ -0,0 +1,6 @@
+#pragma once
+void load_sound();
+void sd_load_sound();
+int play_sound(int id);
+void exit_music();
+int music_SamplePos(int id);
--- source/main.cpp
+++ source/main.cpp
@@ -1,17 +1,11 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <3ds.h>
-#include <citro2d.h>
-#include <limits.h>
-#include <math.h>
-#include <dirent.h>
-#include <unistd.h>
-#include <ctype.h>
+
 #include "main.h"
+#include "audio.h"
 
 // SDカードからテクスチャを読み込む
-const char* texturePath = "sdmc:/3ds/onigiri/image.t3x";
+const char* texturePath = "romfs:/gfx/image.t3x";
 char buffer[BUFFER_SIZE];
 int scene = 0, count = 0;
 
@@ -21,18 +15,21 @@ C2D_SpriteSheet spriteSheet;
 
 int main() {
 	// 初期化
+	romfsInit();
 	gfxInitDefault();
 	C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
 	C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
 	C2D_Prepare();
 	g_dynamicBuf = C2D_TextBufNew(4096);
-
+	gfxSetWide(false);
+	osSetSpeedupEnable(false);
 
 	// 描画バッファ
 	C3D_RenderTarget* top = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
 	C3D_RenderTarget* bot = C2D_CreateScreenTarget(GFX_BOTTOM, GFX_LEFT);
 
 	spriteSheet = C2D_SpriteSheetLoad(texturePath);
+	sd_load_sound();
 
 	while (aptMainLoop()) {
 		hidScanInput();
@@ -43,7 +40,7 @@ int main() {
 		// 描画開始
 		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
 
-		// 上画面に描画
+		// 上画面を描画
 		C2D_TargetClear(top, C2D_Color32(0, 0, 0, 1));
 		C2D_SceneBegin(top);
 		// ステージ
@@ -55,24 +52,27 @@ int main() {
 		switch (scene) {
 		case 0:	//タイトル画面
 
-			C2D_DrawImageAtRotated(C2D_SpriteSheetGetImage(spriteSheet, 1),(float)(TOP_WIDTH/2),(float)(40),0.5f,0,NULL,1.0f,1.0f);
-			C2D_DrawImageAtRotated(C2D_SpriteSheetGetImage(spriteSheet, 2),(float)(TOP_WIDTH/2),(float)(150),0.5f,0,NULL,1.0f,1.0f);
+			C2D_DrawImageAtRotated(C2D_SpriteSheetGetImage(spriteSheet, 1),(float)(TOP_WIDTH/2),(float)(40),0.5f,NULL,1.0f,1.0f);
+			C2D_DrawImageAtRotated(C2D_SpriteSheetGetImage(spriteSheet, 2),(float)(TOP_WIDTH/2),(float)(150),0.5f,NULL,1.0f,1.0f);
 			if (key & KEY_A) scene = 1;
 
 			break;
 
 		case 1:	//ゲーム画面
 
-			C2D_DrawImageAtRotated(C2D_SpriteSheetGetImage(spriteSheet, 2),(float)(TOP_WIDTH/2),(float)(150),0.5f,0,NULL,1.0f,1.0f);
-			C2D_DrawImageAtRotated(C2D_SpriteSheetGetImage(spriteSheet, 3),(float)(80),(float)(TOP_HEIGHT/2),0.5f,0,NULL,1.0f,1.0f);
-			if (key & KEY_X) ++count;
+			C2D_DrawImageAtRotated(C2D_SpriteSheetGetImage(spriteSheet, 2),(float)(TOP_WIDTH/2),(float)(150),0.5f,NULL,1.0f,1.0f);
+			C2D_DrawImageAtRotated(C2D_SpriteSheetGetImage(spriteSheet, 3),(float)(80),(float)(TOP_HEIGHT/2),0.5f,NULL,1.0f,1.0f);
+			if (key & KEY_X) {
+				play_sound(0);
+				++count;
+			}
 
 			snprintf(get_buffer(), BUFFER_SIZE, "%d", count);
-			draw_text(75, (TOP_HEIGHT/2)-10, get_buffer());
+			draw_text(70, (TOP_HEIGHT/2)-10, get_buffer());
 			break;
 		}
 
-		// 下画面に描画（必要に応じて描画）
+		// 下画面を描画（必要に応じて描画）
 		C2D_TargetClear(bot, C2D_Color32(0, 0, 0, 1));
 		C2D_SceneBegin(bot);
 
@@ -81,9 +81,14 @@ int main() {
 	}
 
 	// リソースの解放
+	C2D_TextBufDelete(g_dynamicBuf);
+
 	C2D_Fini();
-	C2D_SpriteSheetFree(spriteSheet);
+	C3D_Fini();
 	gfxExit();
+	romfsExit();
+	exit_music();
+
 	return 0;
 }
 
