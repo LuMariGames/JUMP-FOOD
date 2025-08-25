96539a97dc644de64d84945aec1ab6f74de8f4d3
LuMariGames <kai.hono426@gmail.com>
2025-08-26 07:07:51 +0900

    Update main.h

--- source/main.h
+++ source/main.h
@@ -17,5 +17,5 @@
 #define BOTTOM_HEIGHT 240
 #define BUFFER_SIZE 160
 
-void draw_text(float x, float y, const char *text);
+void draw_text(float x, float y, C2D_Font bcfnt, const char *text);
 char *get_buffer();
